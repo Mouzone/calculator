274676e09a6c297237927d07eb91660921c07958
Mouzone <sunnyliu010@gmail.com>
2024-06-06 11:53:10 -0400

    Linked List implementation

--- linked_list_node.js
+++ linked_list_node.js
@@ -1,7 +1,23 @@
 export default class ListNode {
-    constructor(data=null, type=null, next=null) {
-        this.data = data
+    constructor(val=null, type=null, next=null) {
+        this.val = val
         this.type = type
         this.next = next
     }
+}
+export function parseLinkedList(node) {
+    let left = ""
+    let right = ""
+    let operation =
+    while (node) {
+
+    }
+}
+export function debugListNode(node) {
+    let output_array = []
+    while (node){
+        output_array.push(node.val)
+        node = node.next
+    }
+    console.table(output_array)
 }
--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-import ListNode from "./linked_list_node"
-import operate from "./calculator_func"
+import ListNode, {debugListNode} from "./linked_list_node.js"
+import operate from "./calculator_func.js"
 
-let operation = false
+let operation = false // if operation was ever pressed
 const head = new ListNode()
 let curr_digit = head
 
 const display = document.querySelector(".display")
-let is_display_empty = true
 const buttons = document.querySelectorAll("button")
 buttons.forEach(button => {
     button.addEventListener("click", event => {
-        if (event.currentTarget.classList.contains("clear")) {
+        const curr_button = event.currentTarget
+        if (curr_button.classList.contains("clear")) {
             display.textContent = "0000000000"
-            is_display_empty = true
-        } else if (is_display_empty) {
-            if (event.currentTarget.classList.contains("number")) {
-                display.textContent = event.currentTarget.textContent
-                is_display_empty = false
-                curr_digit.next = new ListNode(event.currentTarget.textContent)
+            debugListNode(head.next)
+            head.next = null
+            curr_digit = head
+        } else if (!head.next) {
+            if (curr_button.classList.contains("number")) {
+                display.textContent = curr_button.textContent
+                curr_digit.next = new ListNode(curr_button.textContent, "number")
                 curr_digit = curr_digit.next
             }
         } else {
-            if (event.currentTarget.classList.contains("number")) {
-                display.textContent += event.currentTarget.textContent
-                curr_digit.next = new ListNode(event.currentTarget.textContent)
+            if (curr_button.classList.contains("number")) {
+                display.textContent += curr_button.textContent
+                curr_digit.next = new ListNode(curr_button.textContent, "number")
                 curr_digit = curr_digit.next
             }
-            if (event.currentTarget.classList.contains("operation")) {
+            if (curr_button.classList.contains("operation")) {
                 if (operation) {
                     if (curr_digit.type === "number") {
                         // submit for operation
@@ -34,8 +35,8 @@ buttons.forEach(button => {
                         // operation to false
                     }
                 } else {
-                    display.textContent += event.currentTarget.textContent
-                    curr_digit.next = new ListNode((event.currentTarget.textContent))
+                    display.textContent += curr_button.textContent
+                    curr_digit.next = new ListNode(curr_button.textContent, "operation")
                     curr_digit = curr_digit.next
                     operation = true
                 }
